249cb581cdf1f91083221d1c5297d26e4d15f18b
miRoox <miroox@outlook.com>
2018-03-28 20:30:55 +0800

    add License to source

--- matlab/bitxor/enRGB.m
+++ matlab/bitxor/enRGB.m
@@ -1,6 +1,21 @@
+%
+%  Copyright 2018 Lu <miroox@outlook.com>
+%
+%  Licensed under the Apache License, Version 2.0 (the "License");
+%  you may not use this file except in compliance with the License.
+%  You may obtain a copy of the License at
+%
+%      http://www.apache.org/licenses/LICENSE-2.0
+%
+%  Unless required by applicable law or agreed to in writing, software
+%  distributed under the License is distributed on an "AS IS" BASIS,
+%  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%  See the License for the specific language governing permissions and
+%  limitations under the License.
+%
+% 输入RGB图像I，将之分解为三个矩阵分别加\解密,然后合成彩色图像
 function [TH]=enRGB(RGB,x,y,k)
 % I=imread('pears.png');
-% 输入RGB图像I，将之分解为三个矩阵分别加\解密,然后合成彩色图像
 R=encrypt(RGB(:,:,1),x,y,k);
 G=encrypt(RGB(:,:,2),x,y,k);
 B=encrypt(RGB(:,:,3),x,y,k);
--- matlab/bitxor/encrypt.m
+++ matlab/bitxor/encrypt.m
@@ -1,9 +1,25 @@
-function [J]=encrypt(I,x0,y0,k0)
+%
+%  Copyright 2018 Lu <miroox@outlook.com>
+%
+%  Licensed under the Apache License, Version 2.0 (the "License");
+%  you may not use this file except in compliance with the License.
+%  You may obtain a copy of the License at
+%
+%      http://www.apache.org/licenses/LICENSE-2.0
+%
+%  Unless required by applicable law or agreed to in writing, software
+%  distributed under the License is distributed on an "AS IS" BASIS,
+%  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+%  See the License for the specific language governing permissions and
+%  limitations under the License.
+%
 % e.g
 % encrypt(rgb2gray(imread('')),6,6,8);
 %
 % 读入原始图像I或加密图像J，必须是灰度图像则返回加密图像或原始图像灰度矩阵；
 % x,y为映射初值，k为一个参数,且k应大于8左右.
+function [J]=encrypt(I,x0,y0,k0)
+
 [m,l]=size(I);
 i=m*l;
 x=zeros(1,i);
